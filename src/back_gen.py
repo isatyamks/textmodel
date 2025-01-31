4e4a6990d2a6710cdcd92ec0264bc87ec25f73fa
isatyamks <isatyamks@gmail.com>
2025-01-31 10:54:09 +0530

    Add background image generation script to create subtle color images

---
+++ src/back_gen.py
@@ -0,0 +1,15 @@
+import os
+import random
+from PIL import Image, ImageDraw, ImageFont
+import textwrap
+
+l = int(input("Enter no of images: "))
+
+def generate_subtle_color():
+    return random.randint(200, 255), random.randint(200, 255), random.randint(200, 255)
+
+for i in range(l):
+    i, j, k = generate_subtle_color()
+    p = f"{i,j,k}"
+    img = Image.new("RGB", (1080, 1080), color=(i, j, k))
+    img.save(f"data\\backgrounds\\{p}.jpg")
