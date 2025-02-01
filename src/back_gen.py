9cd9dbbe4873a263e4cbae7484867aea540d4c2c
isatyamks <isatyamks@gmail.com>
2025-02-01 23:12:09 +0530

    Update .gitignore, remove VSCode settings, and refine color generation in background image script

--- src/back_gen.py
+++ src/back_gen.py
@@ -3,10 +3,17 @@ import random
 from PIL import Image, ImageDraw, ImageFont
 import textwrap
 
+
+
 l = int(input("Enter no of images: "))
 
+
+
 def generate_subtle_color():
-    return random.randint(200, 255), random.randint(200, 255), random.randint(200, 255)
+    return random.randint(100, 155), random.randint(180, 255), random.randint(180, 255)
+
+
+
 
 for i in range(l):
     i, j, k = generate_subtle_color()
