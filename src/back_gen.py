8af7d49fa25930b603aa276eb84a0083e95af6d2
isatyamks <isatyamks@gmail.com>
2025-02-03 23:16:04 +0530

    Refine color generation in background image script for improved subtlety

--- src/back_gen.py
+++ src/back_gen.py
@@ -10,7 +10,7 @@ l = int(input("Enter no of images: "))
 
 
 def generate_subtle_color():
-    return random.randint(100, 155), random.randint(180, 255), random.randint(180, 255)
+    return random.randint(230, 255), random.randint(230, 255), random.randint(230, 255)
 
 
 
@@ -19,7 +19,4 @@ for i in range(l):
     i, j, k = generate_subtle_color()
     p = f"{i,j,k}"
     img = Image.new("RGB", (1080, 1080), color=(i, j, k))
-
-    img = Image.new
-
     img.save(f"data\\backgrounds\\{p}.jpg")
